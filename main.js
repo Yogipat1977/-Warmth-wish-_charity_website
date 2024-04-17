f61e792dfd097bca36dcae439e2574f186479189
patel yogi <yogipatel27072004@gmail.com>
2024-04-17 21:25:57 +0100

    biginning of the our assortments page and added the header in it withe head link as well

--- main.js
+++ main.js
@@ -49,44 +49,6 @@ function stickyNavbar()
  
 
 
-var scroll = window.requestAnimationFrame ||
-            
-             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
-
-function loop() {
-
-  elementsToShow.forEach(function (element) {
-    if (isElementInViewport(element)) {
-      element.classList.add('is-visible');
-    } else {
-      element.classList.remove('is-visible');
-    }
-  });
-
-  scroll(loop);
-}
-
-loop();
-
-
-function isElementInViewport(el) {
-  
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 2
-      && rect.bottom >= 3)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
 
 // Select all div elements
 let divs = document.querySelectorAll('div');
@@ -128,3 +90,14 @@ window.addEventListener('scroll', function()
       navbar.classList.remove('blur'); // Remove blur class from navbar
   }
 });
+
+			function ClickMessage()
+			{
+ 			alert("You have successfully created on click event!");
+		 	}
+
+
+
+
+
+   
